83f8d660364f5bf0b11f06fe6fb5282981d8ff4d
rayhanalshorif133 <rayhanalshorif@gmail.com>
2023-05-07 06:25:40 +0600

    form upload function done

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -6,9 +6,14 @@ const router = express.Router();
 // internal imports
 const { getUsers } = require('../controllers/userController');
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+const avatarUpload = require('../middlewares/users/avatarUpload');
 
 
-// User page
+// User view page
 router.get("/", decorateHtmlResponse("Users"), getUsers);
+
+// add user 
+router.post("/", avatarUpload);
+
 
 module.exports = router;
